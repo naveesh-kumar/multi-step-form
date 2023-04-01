6f78eccc577138faa8bdc0ffe11c08f4d69eafd5
Naveesh Kumar V <naveesh88@gmail.com>
2023-04-01 01:47:23 -0700

    Plan card component created

---
+++ src/components/common/PlanCard.js
@@ -0,0 +1,81 @@
+import { theme } from "../../theme";
+import { Box, Typography, useMediaQuery } from "@mui/material";
+import { withStyles } from "@mui/styles";
+
+const styles = {
+  root: {
+    width: "100px",
+    height: "auto",
+    border: `1px solid ${theme.palette.gray.light}`,
+    borderRadius: "4px",
+    padding: "10px",
+    gap: "30px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    "&:focus": {
+      border: `1px solid ${theme.palette.blue.purplish}`,
+    },
+    "&:hover": {
+      cursor: "pointer",
+    },
+    [theme.breakpoints.down("mobile")]: {
+      width: "100%",
+      flexDirection: "row",
+      alignItems: "start",
+      justifyContent: "start",
+      gap: "15px",
+    },
+  },
+};
+
+const PlanCard = ({
+  classes,
+  imgSrc,
+  planName,
+  planPrice,
+  yearlyBilling,
+  discountMsg,
+}) => {
+  const isMobile = useMediaQuery(theme.breakpoints.down("mobile"));
+  return (
+    <div className={classes.root} tabIndex={1}>
+      <img
+        src={imgSrc}
+        alt=""
+        width={isMobile ? 40 : 30}
+        height={isMobile ? 40 : 30}
+      />
+      <Box>
+        <Box>
+          <Typography
+            variant="subtitle1"
+            fontWeight={600}
+            color={theme.palette.blue.marine}
+            fontSize={isMobile ? 16 : 14}
+            textOverflow="ellipsis"
+          >
+            {planName}
+          </Typography>
+        </Box>
+        <Typography variant="caption" color={theme.palette.gray.cool}>
+          ${planPrice}/{yearlyBilling ? "yr" : "mo"}
+        </Typography>
+        {yearlyBilling && (
+          <Typography
+            variant="subtitle2"
+            fontSize={isMobile ? 14 : 12}
+            color={theme.palette.blue.marine}
+          >
+            {discountMsg}
+          </Typography>
+        )}
+      </Box>
+    </div>
+  );
+};
+
+export default withStyles(styles)(PlanCard);
---
+++ src/stories/PlanCard.stories.js
@@ -0,0 +1,18 @@
+import PlanCard from "../components/common/PlanCard";
+import arcade from "../../public/icons/icon-arcade.svg";
+
+export default {
+  title: "Plan Card",
+  component: PlanCard,
+};
+
+const Template = (args) => <PlanCard {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  imgSrc: arcade,
+  planName: "Arcade",
+  planPrice: "90",
+  yearlyBilling: true,
+  discountMsg: "2 months free",
+};
